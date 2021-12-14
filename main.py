815bad431fef9b7008e16ffa146b0e32d2349428
2damin <leesak2003@naver.com>
2021-12-14 21:45:06 +0900

    add test/train mode

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import os,sys
-import numpy as np
+#import numpy as np
 import torch
 import argparse
 import cv2
+from glob import glob
+import torchsummary
+from model.yolov3 import DarkNet53
 
 def parse_args():
     parser = argparse.ArgumentParser(description="YOLOV3-PYTORCH")
     parser.add_argument('--gpu', dest='gpu', help="GPU to use",
                         default=0, type=int)
     parser.add_argument('--mode', dest='mode', help="train or test",
+                        default=None, type=str)
+    parser.add_argument('--input_dir', dest='inpu_dir', help="input directory",
                         default=None, type=str)
     if len(sys.argv) == 1:
         parser.print_help()
@@ -22,6 +27,8 @@ def train():
 
 def test():
     print("test")
+
+    backbone = DarkNet53(3,5,512,512)
 
 
 if __name__ == "__main__":
