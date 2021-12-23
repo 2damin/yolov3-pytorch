cb8686c06c2ea65b4c6900e6ae0272a779380c32
2damin <leesak2003@naver.com>
2021-12-23 21:41:09 +0900

    add torchsummary

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import torch
 import argparse
 import cv2
 from glob import glob
+from torch.utils.data.dataloader import DataLoader
 import torchsummary as summary
 from model.yolov3 import DarkNet53
 from dataloader.yolodata import Yolodata 
@@ -30,12 +31,17 @@ def test():
     print("test")
 
     train_data = Yolodata(train=True, transform=None)
+    train_loader = DataLoader(train_data, batch_size=1, num_workers=1, pin_memory=True, drop_last=True, shuffle=True)
 
     backbone = DarkNet53(3,5,512,512)
     backbone = backbone.cpu()
-
-    #summary.summary(backbone, input_size=(3, 1280, 704), device='cpu')
-
+    summary.summary(backbone, input_size=(3, 512, 512), device='cpu')
+    for i, batch in enumerate(train_loader):
+        input_img = batch['image']
+        b, c, h, w = input_img.size()
+        #input_ = torch.cat([input_image.unsqueeze(1)])
+        input_ = input_img.view(-1, c, h, w)
+        out = backbone.forward(input_)
 
 if __name__ == "__main__":
     args = parse_args()
