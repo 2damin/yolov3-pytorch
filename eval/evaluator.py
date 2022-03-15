2793c1ac44a3ac8d1286a449368576d18c90613f
2damin <leesak2003@naver.com>
2022-03-16 01:12:50 +0900

    minor update

--- eval/evaluator.py
+++ eval/evaluator.py
@@ -20,31 +20,41 @@ class Evaluator:
 
     def run(self):
         for i, batch in enumerate(self.eval_loader):
-            input_img, targets = batch
-            if self.device == torch.device('cuda'):
-                input_img = input_img.cuda()
+            _input_img, targets = batch
+            #stack [1,c,h,w] images to make [n,c,h,w] image
+            input_img = torch.stack(_input_img,0)
             input_wh = [input_img.shape[3], input_img.shape[2]]
+            
+            input_img = input_img.to(self.device)
 
             with torch.no_grad():
                 start_time = time.time()
             
                 output = self.model(input_img)
                             
-                output_list = self.yololoss.compute_loss(output, targets = None, input_wh = input_wh, yolo_layers = self.model.yolo_layers)
+                output_list = self.yololoss.compute_loss(output,
+                                                         targets = None,
+                                                         nw = self.model.in_width,
+                                                         nh = self.model.in_height,
+                                                         yolo_layers = self.model.yolo_layers)
 
                 output_all = torch.cat(output_list, dim=1)
-                best_box_list = non_max_sup(output_all, self.model.n_classes, conf_th=0.5, nms_th=0.4)
-                #print(best_box_list)
-                
-                self.evaluate(best_box_list, targets)
+                best_box_list = non_max_sup(output_all, self.model.n_classes, conf_th=0.5, nms_th=0.5)
+
+                if best_box_list is None:
+                    continue
+
+                final_box_list = best_box_list[best_box_list[:,4] > 0.85]
+
+                self.evaluate(final_box_list, targets[0])
                 
                 if i % 100 == 0:
                     print("-------{} th iter -----".format(i))
                 
-                if best_box_list is None:
+                if final_box_list is None:
                     continue
                 
-                #drawBox(input_img.detach().cpu().numpy()[0,:,:,:], best_box_list, mode=1)
+                # drawBox(input_img.detach().cpu().numpy()[0,:,:,:], final_box_list, mode=1)
                 
                 #temporary transform the format of GT boxes to draw box in image
                 # for i in range(targets['bbox'][0].shape[0]):
@@ -64,21 +74,38 @@ class Evaluator:
     def evaluate(self, preds, targets):
         if preds is None:
             return
+        #if target object dont exist
+        if targets['bbox'] is None and targets['cls'] is None:
+            return
+        
+        #class mapping
+        class8 = [0,1,2,3,4,5,6,7] #Car, Van, Truck, Ped, Ped_sitting, Cyclist, Tram, Misc
+        class3 = [0,0,0,1,1,2,-1,-1] #Vehicle, Ped, Cyclist
+        class2 = [0,0,0,1,1,1,-1,-1] #Vehicle, Ped
+        
         #move the preds and targets from device to cpu
         preds = preds.detach().cpu()
         targets['bbox'] = targets['bbox'].detach().cpu()
         targets['cls'] = targets['cls'].detach().cpu()
         #remove ignore class GT data
-        targets_bbox_valid = targets['bbox'][0][targets['cls'][0] != 8]
-        targets_cls_valid = targets['cls'][0][targets['cls'][0] != 8]
+        targets_cls_valid = None
+        targets_bbox_valid = None
+        for tidx in range(targets['cls'].shape[0]):
+            if targets['cls'][tidx] == 8 or targets['occ'][tidx] > 1 or targets['trunc'][tidx] > 0.25:
+                continue
+            targets_cls_valid = targets['cls'][tidx].unsqueeze(0) if targets_cls_valid is None else torch.cat((targets_cls_valid, targets['cls'][tidx].unsqueeze(0)), dim=0)
+            targets_bbox_valid = targets['bbox'][tidx].unsqueeze(0) if targets_bbox_valid is None else torch.cat((targets_bbox_valid, targets['bbox'][tidx].unsqueeze(0)), dim=0)
+
+
         #make mask tensor 
         pred_mask = torch.ones(preds.shape[0], requires_grad=False)
-        gt_mask = torch.zeros(targets_cls_valid.shape[0], requires_grad=False)
-        #print("pred_mask shape : ", pred_mask.shape, "gt_mask shape : ", gt_mask.shape)
-        
-        for i in range(targets_bbox_valid.shape[0]):
+        gt_mask = torch.zeros(targets_cls_valid.shape[0], requires_grad=False) if targets_cls_valid is not None else torch.tensor([])
+
+        target_num = targets_bbox_valid.shape[0] if targets_bbox_valid is not None else 0
+        for i in range(target_num):
             tbox = targets_bbox_valid
-            tcls = targets_cls_valid
+            tcls = [class3[tcls] for tcls in targets_cls_valid]
+
             #change the target box format cxcywh to minmax
             cxcy2minmax(tbox[i])
             tbox[i,0] = tbox[i,0] * self.model.in_width
@@ -88,13 +115,14 @@ class Evaluator:
     
             for j, (pbox, pobj_score, pcls_score, pcls_idx) in enumerate(zip(preds[:,:4], preds[:,4:5], preds[:,5:6], preds[:,6:])):
                 #print(pbox.shape, pobj_score.shape, pcls_score.shape, pcls_idx.shape, tbox.shape, tcls.shape)
-                
-                if tcls[i] != pcls_idx or pred_mask[j] == 0:
+
+                pcls_idx = class3[int(pcls_idx.item())]
+                if tcls[i] != pcls_idx or pred_mask[j] == 0 or tcls[i] == -1 or pcls_idx == -1:
                     continue
                 
-                iou_value = iou(tbox[i:i+1], preds[j:j+1,:4], mode=1)
+                iou_value = iou(tbox[i:i+1], pbox.unsqueeze(0), mode=1)
                 
-                #print("box {} {} / iou : {}".format(tbox[i:i+1], preds[j:j+1,:4], iou_value))
+                #print("box {} {} / iou : {}".format(tbox[i:i+1], pbox, iou_value))
 
                 if iou_value > 0.5:
                     gt_mask[i] = 1
