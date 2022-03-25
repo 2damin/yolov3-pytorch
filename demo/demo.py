34019e6ee58a3acd07418ebf3e09dc5b1d80495d
2damin <leesak2003@naver.com>
2022-03-26 01:06:09 +0900

    update demo

--- demo/demo.py
+++ demo/demo.py
@@ -17,27 +17,25 @@ class Demo:
             
             input_img = input_img.to(self.device, non_blocking=True)
 
+            num_batch = input_img.shape[0]
             with torch.no_grad():
                 start_time = time.time()
             
                 output = self.model(input_img)
                             
                 # _, output_list = self.yololoss.compute_loss(output, targets = None, nw = nw, nh = nh, yolo_layers = self.model.yolo_layers)
+                best_box_list = non_max_suppression(output, conf_thres=0.25, iou_thres=0.45)
 
-                output_all = torch.cat(output, dim=1)
-                print(output_all.shape)
-                best_box_list = non_max_sup(output_all, self.model.n_classes, conf_th=0.5, nms_th=0.5)
-                
-                if best_box_list is None:
-                    continue
-                print(best_box_list.shape)
-                final_box_list = best_box_list[best_box_list[:,4] > 0.85]
-                print("final :", final_box_list.shape)
-                if i % 100 == 0:
-                    print("-------{} th iter -----".format(i))
-                if final_box_list is None:
-                    continue
-                show_img = input_img.detach().cpu().numpy()[0,:,:,:]
-                drawBox(show_img, best_box_list, best_box_list[:,6], mode=1)
+                for b in range(num_batch):
+                                    
+                    if best_box_list[b] is None:
+                        continue
+                    final_box_list = [bbox for bbox in best_box_list[b] if bbox[4] > 0.5]
+                    print("final :", len(final_box_list))
+
+                    if final_box_list is None:
+                        continue
+                    show_img = input_img[b].detach().cpu().numpy()
+                    drawBoxlist(show_img, final_box_list, mode=1)
 
         
