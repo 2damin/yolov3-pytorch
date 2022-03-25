b7a0f8d57df6b68fa531e2f1ffd0dfdd6d05dcbe
2damin <leesak2003@naver.com>
2022-03-26 01:04:20 +0900

    add torch2onnx mode

--- main.py
+++ main.py
@@ -51,6 +51,10 @@ def parse_args():
 
 def collate_fn(batch):
     batch = [data for data in batch if data is not None]
+    #skip invalid frames
+    if len(batch) == 0:
+        return
+
     imgs, targets, anno_path = list(zip(*batch))
 
     imgs = torch.stack([img for img in imgs])
@@ -197,7 +201,7 @@ def eval(cfg_param = None, using_gpus = None):
     
     print("torch output : ", len(torch_outs), " ", torch_outs[0].shape)
     print("onnx out: ", len(ort_outs), ort_outs[0].shape)
-    torch_np_outs = to_numpy(torch_outs[2])
+    #torch_np_outs = to_numpy(torch_outs[2])
     # ONNX 런타임과 PyTorch에서 연산된 결과값 비교
     #np.testing.assert_allclose(torch_np_outs, ort_outs[2], rtol=1e-03, atol=1e-05)
 
@@ -235,7 +239,77 @@ def demo(cfg_param = None, using_gpus = None):
     
     demo.run()
 
+
+#convert trained yolov3 model from pytorch to ONNX format 
+def torch2onnx(cfg_param = None, using_gpus = None):
+    #Get OD model
+    cfg_param['batch'] = 1
+    model = DarkNet53(args.cfg, cfg_param)
+    
+    #Set the device what you use, GPU or CPU
+    for i in using_gpus:
+        print("GPU total memory : {} free memory : {}".format(get_memory_total_MiB(i), get_memory_free_MiB(i)))
+        if get_memory_free_MiB(i) / get_memory_total_MiB(i) < 0.5:
+            print("Avaliable memory is {}%, GPU is already used now, Exit process".format(get_memory_free_MiB(i) / get_memory_total_MiB(i)))
+            sys.exit(1)
+    if len(using_gpus) == 1:
+        device = torch.device("cuda:"+str(using_gpus[0]) if torch.cuda.is_available() else "cpu")
+        torch.cuda.set_device(using_gpus[0])
+        model = torch.nn.DataParallel(model, device_ids=using_gpus)
+        model = model.cuda()
+    elif len(using_gpus) == 0:
+        print("Disable to use GPU. Exit process")
+        device = torch.device("cpu")
+        model = model.to(device)
+    elif len(using_gpus) > 1:
+        print("using_gpus : {}".format(using_gpus))
+        device = torch.device("cuda:"+str(using_gpus[0]) if torch.cuda.is_available() else "cpu")
+        model = torch.nn.DataParallel(model, device_ids=using_gpus)
+        model = model.cuda()
+        model.to(f'cuda:{model.device_ids[0]}')
+
+    torch.backends.cudnn.benchmark = True
+
+    #If checkpoint is existed, load the previous checkpoint.
+    checkpoint = None
+    if args.checkpoint is not None:
+        print("load pretrained model ", args.checkpoint)
+        checkpoint = torch.load(args.checkpoint)
+        for key, value in checkpoint['model_state_dict'].copy().items():
+            new_key = "module." + key
+            checkpoint['model_state_dict'][new_key] = checkpoint['model_state_dict'].pop(key)
+        model.load_state_dict(checkpoint['model_state_dict'])
+
+    if len(using_gpus) > 0:
+        yolo_model = model.module
+    else:
+        yolo_model = model
+    
+    yolo_model.eval()
         
+    x_test = torch.randn(1, 3, cfg_param["in_width"], cfg_param["in_height"], requires_grad=True).to(device)
+    torch.onnx.export(yolo_model, x_test, "yolov3.onnx", export_params=True, opset_version=11, input_names=['input'], output_names=['output'] )
+
+
+    def to_numpy(tensor):
+        return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+    ort_session = onnxruntime.InferenceSession("./yolov3.onnx",providers=['TensorrtExecutionProvider', 'CUDAExecutionProvider', 'CPUExecutionProvider'])
+
+    # ONNX 런타임에서 계산된 결과값
+    ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x_test)}
+    
+    ort_outs = ort_session.run(None, ort_inputs)
+    
+    torch_outs = yolo_model(x_test)
+    
+    print("torch output : ", len(torch_outs), " ", torch_outs.shape)
+    print("onnx out: ", len(ort_outs), ort_outs[0].shape)
+    
+    # ONNX 런타임과 PyTorch에서 연산된 결과값 비교
+    torch_np_outs = to_numpy(torch_outs)
+    np.testing.assert_allclose(torch_np_outs, ort_outs[0], rtol=1e-03, atol=1e-05)
+
 if __name__ == "__main__":
     args = parse_args()
     cfg_data = parse_hyperparam_config(args.cfg)
@@ -251,7 +325,8 @@ if __name__ == "__main__":
         eval(cfg_param, using_gpus)
     elif args.mode == "demo":
         demo(cfg_param, using_gpus)
+    elif args.mode == "onnx":
+        torch2onnx(cfg_param, using_gpus)
     else:
         print("Unknown mode error")
-
     print("finish")
